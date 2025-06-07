2263ce089f8104cf3a424b5679e01059c3ab7fc1
Yizhou Li <yizhoul316@hotmail.com>
2025-06-07 09:13:14 -0400

    feat: adjust first migration

--- app/db/models.py
+++ app/db/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, Numeric, DateTime
+from sqlalchemy import Column, Integer, String, Numeric, DateTime, func
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -8,9 +8,9 @@ Base = declarative_base()
 class QueryHistory(Base):
     __tablename__ = "query_history"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
     source = Column(String, nullable=False)
     destination = Column(String, nullable=False)
     kilometers = Column(Numeric(10, 2), nullable=False)
     miles = Column(Numeric(10, 2), nullable=False)
-    created_at = Column(DateTime(timezone=True), nullable=False, default=datetime.utcnow) 
+    created_at = Column(DateTime(timezone=True), nullable=False,  server_default=func.now()) 
